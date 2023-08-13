ed66f228f1d183c91f5618fd3967690fc9af1431
Moisés Villarroel <140099979+MoisesV2605@users.noreply.github.com>
2023-08-13 14:33:24 -0400

    Se creó en su mayoria la base de la pagina externa

---
+++ externa/src/components/Barra_Superior/Barra_Superior.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Barra_Superior.css'; // Importa el archivo CSS para los estilos
+import logo from '../Images/logo2.png';
+import Barra_Titulo from '../Barra_Titulo/Barra_Titulo';
+
+function Barra_Superior() {
+  return (
+    <div className="barra-superior">
+      
+    <img className='logo' src={logo} alt="" />
+   <div>
+    
+   </div>
+    </div>
+      
+  
+  );
+}
+
+export default Barra_Superior;
