e93bf031f452c154c4b8727adc638c2310a0e880
Rosibel Cordero <140108986+csrosibel@users.noreply.github.com>
2023-08-14 00:39:46 -0400

    Update Login.jsx

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -4,8 +4,22 @@ import { Link } from 'react-router-dom'
 function Login() {
   return (
     <div>
-        Login
-
+         <div className='contenedor-login'>
+          <div className='contenedor-formulario'>
+            <h1>Bienvenido</h1>
+            <h2>Inicia sesión para continuar</h2>
+            <h3>Usuario</h3>
+            <form>
+              <input type = 'text' className='input1' />
+            </form>
+            <h3>Contraseña</h3>
+            <form>
+              <input type = 'password' className='input1' />
+            </form>
+            <button>Iniciar sesion</button> 
+            <a href='#'>¿Olvidaste la contraseña?</a>
+          </div>
+        </div>
         <Link to="/recuperar_password">Recuperar Contraseña</Link>
     </div>
   )
