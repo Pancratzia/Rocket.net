678704ca013443820c8f46143869baaab9551e22
Juan Villegas <juancvillefer@gmail.com>
2023-08-17 20:18:13 -0400

    Anexo funcion de auditar y elimino API para crear auditoria elimino carpmeto Middleware

--- backend/routers/auditoria.js
+++ backend/routers/auditoria.js
@@ -1,34 +1,12 @@
+
 const express = require('express');
 const cors = require('cors');
 const pool = require('../database/db.js');
-
-const { validateAudit } = require('../validaciones/validAudit.js');
-const {obtenerFechayHora} = require('../funciones/funciones.js');
-
 
 const routerAuditoria= express.Router();
 
 routerAuditoria.use(express.json());
 routerAuditoria.use(cors());
-
-// create 
-
-routerAuditoria.post('/', validateAudit,  async(req, res) => { 
-
-  try {
-      const {operacion, id_usuario} = req.body;
-      const fechaActual = obtenerFechayHora("fecha");
-      const horaActual = obtenerFechayHora("hora");
-
-      const newAudit = await pool.query("INSERT INTO auditoria (operacion,id_usuario,fecha,hora) VALUES (UPPER($1),$2,CAST($3 as date),CAST($4 as time))",
-        [operacion,id_usuario,fechaActual,horaActual]);
-      res.json(newAudit.rows[0]);
-          
-  } catch (err) {
-      console.error(err.message);
-  }
-});
-
 
 
 // get all
@@ -40,22 +18,6 @@ routerAuditoria.post('/', validateAudit,  async(req, res) => {
 
 
 
-
-
-// get by id
-
-
-
-
-
-
-
-
-
-
-
-
-// get por rango de fecha
 
 
 
--- backend/routers/puntos.js
+++ backend/routers/puntos.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const pool = require('../database/db.js');
 
 const { validatePuntos } = require('../validaciones/validPoints.js');
-
+const {auditar} = require('../funciones/funciones.js')
 
 const routerPuntos= express.Router();
 routerPuntos.use(express.json());
@@ -14,8 +14,14 @@ routerPuntos.use(cors());
 routerPuntos.post('/',validatePuntos, async(req,res)=>{
     try {
         const {id_zona,latitud,longitud} = req.body;
+        const  operacion  = req.method;
+        const  id_usuario  =req.headers['id_usuario'];
+
         const newPuntos = await pool.query("INSERT INTO puntos (id_zona,latitud,longitud) VALUES($1,$2,$3)",
             [id_zona, latitud, longitud]);
+
+            auditar(operacion,id_usuario);
+        
         res.json(newPuntos.rows[0])
     } catch (err) {
         console.error(err.message);
