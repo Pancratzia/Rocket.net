57f2d887bb38adfbce84a3dec314dc0ad134274f
Maria Castillo <marfcastillo@users.noreply.github.com>
2023-08-22 00:34:09 -0400

    Diseño del Dashboard. Sin responsive y lado superior derecho

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./navbar.scss";
+
+function Navbar(){
+    return(
+    <header>
+     <nav className="navbar">
+     </nav>   
+     </header>
+    )
+}
+
+export default Navbar;
