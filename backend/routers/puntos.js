7b3bf6938cad960b1e96c5f65efffc1add94ebdb
Juan Villegas <juancvillefer@gmail.com>
2023-08-21 23:56:48 -0400

    modificacion puntos

--- backend/routers/puntos.js
+++ backend/routers/puntos.js
@@ -17,19 +17,29 @@ routerPuntos.post('/',validaPuntos, async(req,res)=>{
         const  operacion  = req.method;
         const  id_usuarioAuditoria  =req.headers['id_usuario'];
 
+        // Validaciones para validar existencia del poligono
         const buscarIdPoligono = await pool.query("SELECT id_poligono FROM poligonos WHERE id_poligono = $1",[id_poligono]);
 
         if (buscarIdPoligono.rowCount === 0) {
-          // No se eliminó ningún polígono, ya que no se encontró en la base de datos
           return res.status(404).json({ error: 'Polígono no encontrado' });
         }
 
+        //Validaciones para validar que un punto no tenga la misma latitud y longitud 
+        const existenciasPuntos = await pool.query("SELECT id_punto FROM puntos WHERE id_poligono = $1 AND latitud = $2 AND longitud = $3",
+        [id_poligono, latitud, longitud]);
+
+        if (existenciasPuntos.rows.length > 0) {
+        return res.status(400).send({error: "Ya existe este punto en el polígono"});
+        }
+
+        // Insertar puntos
         await pool.query("INSERT INTO puntos (id_poligono,latitud,longitud) VALUES($1,$2,$3) RETURNING *",
             [id_poligono, latitud, longitud]);
 
-            auditar(operacion,id_usuarioAuditoria);
-        
-            return res.status(200).json({ mensaje: 'Punto creado exitosamente' });
+        auditar(operacion,id_usuarioAuditoria);
+
+        return res.status(200).json({ mensaje: 'Punto creado exitosamente' });
+
     } catch (err) {
         console.error(err.message);
     }
