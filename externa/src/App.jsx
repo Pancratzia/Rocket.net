17d0b6282c6fd515031e6a0be1b1852adf2c5263
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-08-23 18:29:26 -0400

    Se muestra el mapa con los poligonos

--- externa/src/App.jsx
+++ externa/src/App.jsx
@@ -1,37 +1,29 @@
-
-import './App.css'
-import Barra_Superior from './components/Barra_Superior/Barra_Superior.jsx'
-import Barra_Titulo from './components/Barra_Titulo/Barra_Titulo';
-import Footer from './components/Footer/Footer';
-import Panel from './components/Panel/Panel';
-
-
-
+import "./App.css";
+import Barra_Superior from "./components/Barra_Superior/Barra_Superior.jsx";
+import Barra_Titulo from "./components/Barra_Titulo/Barra_Titulo";
+import Footer from "./components/Footer/Footer";
+import Panel from "./components/Panel/Panel";
 
 function App() {
-
   return (
     <>
-
-
       <header className="logo">
         <Barra_Superior />
       </header>
 
-      <section className='banner'>
-      <Barra_Titulo />
+      <section className="banner">
+        <Barra_Titulo />
       </section>
 
       <main>
-    <Panel />
+        <Panel />
       </main>
 
-      <footer className='footer'>
- <Footer />
-        </footer>
-   
-</>
-  )
+      <footer className="footer">
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
 export default App;
--- externa/src/components/Panel/Panel.jsx
+++ externa/src/components/Panel/Panel.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
-import '../styles/Panel.css'; // Importa el archivo CSS para los estilos
-import mapa from '../Images/mapa.jpg';
-import { AiOutlineSearch } from 'react-icons/ai';
-import Mapview from '../Mapa/Mapview';
+import React from "react";
+import "../styles/Panel.css"; // Importa el archivo CSS para los estilos
+import Mapa from "../Mapa/Mapa";
 
 function Panel() {
   return (
     <div className="panel">
       <div className="division-izquierda">
-        <Mapview/>
+        <Mapa />
       </div>
       <div className="division-derecha">
-        <p className='texto-principal'>¡Conoce si estás dentro de las zonas de Cobertura que cubre Rocket.net!
-          <div>
+        <p className="texto-principal">
+          ¡Conoce si estás dentro de las zonas de Cobertura que cubre
+          Rocket.net!
+          
             <br />
-
-          </div>
-
-          Somos la empresa de telecomunicaciones en la que puedes confiar. Garantizamos rapidez, eficiencia y el mejor servicio al cliente. ¡Te Esperamos!
+          
+          Somos la empresa de telecomunicaciones en la que puedes confiar.
+          Garantizamos rapidez, eficiencia y el mejor servicio al cliente. ¡Te
+          Esperamos!
         </p>
-        <div className='buscador'>
-          <button className='boton-buscador'>Buscar</button>
+        <div className="buscador">
+          <button className="boton-buscador">Buscar</button>
         </div>
-        <p className='texto-secundario'>**Estas areas de cobertura pueden sufrir cambios para el incremento del alcance de las coberturas</p>
-        <button className='boton-contactanos'>Contactanos</button>
+        <p className="texto-secundario">
+          **Estas areas de cobertura pueden sufrir cambios para el incremento
+          del alcance de las coberturas
+        </p>
+        <button className="boton-contactanos">Contactanos</button>
       </div>
     </div>
   );
