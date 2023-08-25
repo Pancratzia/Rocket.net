a8dfe2d8262cbad61d75447d1abf838d2cddab34
Juan Villegas <juancvillefer@gmail.com>
2023-08-24 22:26:17 -0400

    integracion de la API en el component mapa

--- externa/src/components/Mapa/Mapa.jsx
+++ externa/src/components/Mapa/Mapa.jsx
@@ -13,67 +13,7 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-const data = {
-  type: "FeatureCollection",
-  features: [
-    {
-      type: "Feature",
-      properties: {
-        name: "Poligono 1",
-      },
-      geometry: {
-        coordinates: [
-          [
-            [-69.36667928156903, 10.069865576316772],
-            [-69.36624736588685, 10.062380875843743],
-            [-69.3599413969249, 10.048006362344225],
-            [-69.35311712914381, 10.047921304148645],
-            [-69.35648607146587, 10.064932497776695],
-            [-69.36667928156903, 10.069865576316772],
-          ],
-        ],
-        type: "Polygon",
-      },
-    },
-    {
-      type: "Feature",
-      properties: {
-        name: "Poligono 2",
-      },
-      geometry: {
-        coordinates: [
-          [
-            [-69.30958002836468, 10.073097552397883],
-            [-69.30828428131763, 10.060509673609943],
-            [-69.29333999870886, 10.056086788871795],
-            [-69.29031658893254, 10.073437758523625],
-            [-69.30958002836468, 10.073097552397883],
-          ],
-        ],
-        type: "Polygon",
-      },
-    },
-    {
-      type: "Feature",
-      properties: {
-        name: "Poligono 3",
-      },
-      geometry: {
-        coordinates: [
-          [
-            [-69.35553585696508, 10.084154067702016],
-            [-69.34240562022202, 10.0755640386718],
-            [-69.31424471773379, 10.084749407763908],
-            [-69.28867530933974, 10.079306257679491],
-            [-69.31433110087023, 10.09061770092363],
-            [-69.35553585696508, 10.084154067702016],
-          ],
-        ],
-        type: "Polygon",
-      },
-    },
-  ],
-};
+
 
 const invertCoordinates = (coordinates) =>
   coordinates.map((coord) => [coord[1], coord[0]]);
@@ -98,7 +38,7 @@ function ChangeView({ center, zoom }) {
 function Mapa({ currentPosition }) {
   const [markerPosition, setMarkerPosition] = useState([10.0736, -69.3214]);
   const [mapCenter, setMapCenter] = useState([10.0736, -69.3214]);
-  const [data, setData] = useState({});
+  const [data, setData] = useState({ type: "FeatureCollection", features: [] });
 
   const handleMapClick = (lat, lng) => {
     setMarkerPosition([lat, lng]);
@@ -109,7 +49,7 @@ function Mapa({ currentPosition }) {
 
     axios.get('http://localhost:3000/api/poligonospuntos')
     .then(res => {
-        setData(res.data.Features);
+        setData(res.data);
       })
     .catch(error => {
         console.error('Error al obtener los datos de la API', error);
