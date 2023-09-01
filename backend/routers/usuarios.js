46bf1b608b96cbdcf2a3df97ad8831751fa5448f
Carlos Escalona <140100221+CalOwO278@users.noreply.github.com>
2023-09-01 14:01:49 -0400

    usuario

--- backend/routers/usuarios.js
+++ backend/routers/usuarios.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 const cors = require('cors');
 const pool = require('../database/db.js');
 const bcrypt = require('bcrypt');
-
 const crypto = require('crypto');
 
 
