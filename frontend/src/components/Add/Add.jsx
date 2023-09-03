7f297b67312b0c9f340c9c0305cd790aa5b883ff
Moisés Villarroel <140099979+MoisesV2605@users.noreply.github.com>
2023-09-02 22:19:50 -0400

    Props creados de modal y responsive basico (falta)

--- frontend/src/components/Add/Add.jsx
+++ frontend/src/components/Add/Add.jsx
@@ -1,86 +1,143 @@
-import React from 'react';
+import React from "react";
+
 import "./Add.css";
 
-function Add(props) {
-  const { onSave, onCancel } = props;
+
+const Add = ({ 
+  estado,
+  cambiarEstado,
+  titulo,
+  campo1, idCampo1,
+  typeCampo1,
+  campo2,
+  idCampo2,
+  typeCampo2,
+  campo3,
+  idCampo3,
+  typeCampo3,
+  campo4,
+  idCampo4,
+  typeCampo4,
+  campo5,
+  idCampo5,
+  typeCampo5,
+  campo6,
+  idCampo6,
+  typeCampo6,
+  campo7,
+  idCampo7,
+  typeCampo7,
+  campo8,
+  idCampo8,
+  typeCampo8,
+  campo9,
+  idCampo9,
+  typeCampo9,
+  campo10,
+  idCampo10,
+  typeCampo10,
+  campo11,
+  idCampo11,
+  typeCampo11,
+  campo12,
+  idCampo12,
+  typeCampo12 
+}) => {
+
 
   return (
-    <div className="modal-container">
-      <div className="modal">
-        <h2>Agregar Usuario</h2>
+    <>
+      {estado &&
+        <div className="superposiciones">
+          <div className="contenedor-modal">
+            <div className="parte-arriba">
+              <div className="titulo">
+                <h3>{titulo}</h3>
+              </div>
+              <div>
+                <button className="boton-cerrar-primario" onClick={() => cambiarEstado(!estado)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <form className="form-grid">
+              <div className='formulario-input'>
+                <div className="form-row">
+                  <label htmlFor={idCampo1}>{campo1}</label>
+                  <input type={typeCampo1} id={idCampo1} />
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo2}>{campo2}</label>
+                  <input type={typeCampo2} id={idCampo2} />
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo3}>{campo3}</label>
+                  <input type={typeCampo3} id={idCampo3} />
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo4}>{campo4}</label>
+                  <input type={typeCampo4} id={idCampo4} />
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo5}>{campo5}</label>
+                  <input type={typeCampo5} id={idCampo5} />
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo6}>{campo6}</label>
+                  <input type={typeCampo6} id={idCampo6} />
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo7}>{campo7}</label>
+                  <input type={typeCampo7} id={idCampo7} />
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo8}>{campo8}</label>
+                  <select id={idCampo8}>
+                    <option value="tipo1">Tipo 1</option>
+                    <option value="tipo2">Tipo 2</option>
+                  </select>
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo9}>{campo9}</label>
+                  <select id={idCampo9}>
+                    <option value="tipo1">Departamento 1</option>
+                    <option value="tipo2">Departamento 2</option>
+                  </select>
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo10}>{campo10}</label>
+                  <input type={typeCampo10} id={idCampo10} />
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo11}>{campo11}</label>
+                  <select id={idCampo11}>
+                    <option value="pregunta1">Pregunta 1</option>
+                    <option value="pregunta2">Pregunta 2</option>
+                  </select>
+                </div>
+                <div className="form-row">
+                  <label htmlFor={idCampo12}>{campo12}</label>
+                  <input type={typeCampo12} id={idCampo12} />
+                </div>
+              </div>
+              <div className="botones">
+                <div className='boton-guardar'>
+                  <button type="submit">Guardar</button>
+                </div>
 
-        <form className="form-grid">
-          <div className='formulario-input'>
-            <div className="form-row">
-              <label htmlFor="nombre">Nombre: </label>
-              <input type="text" id="nombre" />
-            </div>
-          <div className="form-row">
-            <label htmlFor="apellido">Apellido:</label>
-            <input type="text" id="apellido" />
-          </div>
-          <div className="form-row">
-            <label htmlFor="extencionTelefonica">Extención telefonica:</label>
-            <input type="text" id="extencionTelefonica" />
-          </div>
-          <div className="form-row">
-            <label htmlFor="telefono">Número telefónico:</label>
-            <input type="number" id="telefono" />
-          </div>
-          <div className="form-row">
-            <label htmlFor="cedula">Cédula:</label>
-            <input type="number" id="cedula" />
-          </div>
-          <div className="form-row">
-            <label htmlFor="correo">Correo:</label>
-            <input type="email" id="correo" />
-          </div>
-          <div className="form-row">
-            <label htmlFor="clave">Clave:</label>
-            <input type="text" id="clave" />
-          </div>
-          <div className="form-row">
-            <label htmlFor="tipoUsuario">Tipo:</label>
-            <select id="tipoUsuario">
-              <option value="tipo1">Tipo 1</option>
-              <option value="tipo2">Tipo 2</option>
-            </select>
-          </div>
-          <div className="form-row">
-            <label htmlFor="departamento">Departamento:</label>
-            <select id="departamento">
-              <option value="tipo1">Departamento 1</option>
-              <option value="tipo2">Departamento 2</option>
-            </select>
-          </div>
-          <div className="form-row">
-            <label htmlFor="usuario">Usuario:</label>
-            <input type="text" id="usuario" />
-          </div>
-          <div className="form-row">
-            <label htmlFor="pregunta">Pregunta:</label>
-            <select id="pregunta">
-              <option value="pregunta1">Pregunta 1</option>
-              <option value="pregunta2">Pregunta 2</option>
-            </select>
-          </div>
-          <div className="form-row">
-            <label htmlFor="respuesta">Respuesta:</label>
-            <input type="text" id="respuesta" />
-          </div>
-          </div>
-          <div className="botones">
-            <div className='boton-guardar'>
-              <button type="submit" onClick={onSave}>Guardar</button>
-            </div>
+                <div className="boton-cerrar" onClick={() => cambiarEstado(!estado)}>
+                  <button type="button">Cancelar</button>
+                </div>
+              </div>
+            </form>
 
-            <div className="boton-cerrar">
-              <button type="button" onClick={onCancel}>Cancelar</button>
-            </div>
           </div>
-        </form>
-      </div>
-    </div>
+
+        </div>
+      }
+    </>
   );
 }
 
--- frontend/src/pages/gestion_usuarios/GestionUsuarios.jsx
+++ frontend/src/pages/gestion_usuarios/GestionUsuarios.jsx
@@ -1,99 +1,152 @@
 import React from 'react';
 import "./GestionUsuarios.css";
-import Tabla  from '../../components/Tabla/Tabla';
+import Tabla from '../../components/Tabla/Tabla';
 import Add from '../../components/Add/Add';
 import { useState } from 'react';
 
-function GestionUsuarios(){
+function GestionUsuarios() {
 
-    const columnas  = [
-        {
-        field: 'foto_usuario',
-        headerName: 'Foto',
-        width: 80,
-        },
-        {
-        field: 'usuario', //campo
-        headerName: 'Usuario',  //nombre de la columna
-        width: 90, // el ancho que ocupa
-        },
+  const columnas = [
+    {
+      field: 'foto_usuario',
+      headerName: 'Foto',
+      width: 80,
+    },
+    {
+      field: 'usuario', //campo
+      headerName: 'Usuario',  //nombre de la columna
+      width: 90, // el ancho que ocupa
+    },
 
-        {
-        field: 'sede_departamento',
-        headerName: 'Departamento',
-        width: 150,    
-        },
+    {
+      field: 'sede_departamento',
+      headerName: 'Departamento',
+      width: 150,
+    },
 
-        {
-        field: 'tipo_usuario',
-        headerName: 'Tipo',
-        width: 80,
-        },
+    {
+      field: 'tipo_usuario',
+      headerName: 'Tipo',
+      width: 80,
+    },
 
-        {
-        field: 'nombre_usuario',
-        headerName: 'Nombre',
-        width: 150,
-        },
+    {
+      field: 'nombre_usuario',
+      headerName: 'Nombre',
+      width: 150,
+    },
 
-        {
-        field: 'apellido_usuario',
-        headerName: 'Apellido',
-        width: 150,
-         },
-        
-        {
-        field: 'pregunta_usuario',
-        headerName: 'Pregunta de seguridad',
-        width: 190,
-        },
+    {
+      field: 'apellido_usuario',
+      headerName: 'Apellido',
+      width: 150,
+    },
 
-        {
-        field: 'extension_usuario',
-        headerName: 'Extension telefonica',
-        width: 180,
-        },
-        {
-        field: 'accion_usuario',
-        headerName: 'Accion',
-        width: 100,
-        }
-    ]
+    {
+      field: 'pregunta_usuario',
+      headerName: 'Pregunta de seguridad',
+      width: 190,
+    },
 
-    const filas = [];
+    {
+      field: 'extension_usuario',
+      headerName: 'Extension telefonica',
+      width: 180,
+    },
+    {
+      field: 'accion_usuario',
+      headerName: 'Accion',
+      width: 100,
+    }
+  ]
 
-  const [showAddModal, setShowAddModal] = useState(false);
 
-  const handleOpenAddModal = () => {
-    setShowAddModal(true);
-  };
 
-  const handleSave = () => {
-    // Lógica para guardar el usuario
-    setShowAddModal(false);
-  };
 
-  const handleCancel = () => {
-    setShowAddModal(false);
-  };
+  const [estadoModal, cambiarEstadoModal] = useState(false);
+
+
+
+ 
 
   return (
-    <div className='contenedor-gestion'>
-      <div className='titulo'>
-        <h1>Gestion de usuarios</h1>
-      </div>
-      <div className='contenedor-busqueda'>
-        <button className='boton' onClick={handleOpenAddModal}>Agregar</button>
-        <button className='boton boton-editar' onClick={handleOpenAddModal}>Editar</button>
-      </div>
 
-      {/* <div className='contenedor-tabla'>
-        <Tabla columns={columnas} rows={filas} />
-      </div> */}
+    <div>
 
-      {showAddModal && <Add onSave={handleSave} onCancel={handleCancel} />}
+      
+
+
+
+      <div className='contenedor-gestion'>
+        <div className='titulo'>
+          <h1>Gestion de usuarios</h1>
+        </div>
+        <div className='contenedor-busqueda'>
+          <button className='boton' onClick={() => cambiarEstadoModal(!estadoModal)}>Agregar</button>
+  
+        </div>
+
+        <Add 
+        estado={estadoModal} 
+        cambiarEstado={cambiarEstadoModal}
+        titulo="Agregar Usuario"
+
+        campo1="Nombre:"
+        idCampo1="nombre"
+        typeCampo1="text"
+
+        campo2="Apellido:"
+        idCampo2="apellido"
+        typeCampo2="text"
+
+        campo3="Extensión telefonica:"
+        idCampo3="extension"
+        typeCampo3="number"
+
+        campo4="Número telefonico:"
+        idCampo4="telefono"
+        typeCampo4="number"
+
+        campo5="Cedula:"
+        idCampo5="nombre"
+        typeCampo5="number"
+
+        campo6="Correo:"
+        idCampo6="correo"
+        typeCampo6="email"
+
+        campo7="Clave:"
+        idCampo7="clave"
+        typeCampo7="varchar"
+
+        campo8="Tipo de Usuario:"
+        idCampo8="tipo"
+        typeCampo8=""
+
+        campo9="Departamento:"
+        idCampo9="departamento"
+        typeCampo9=""
+
+        campo10="Usuario:"
+        idCampo10="usuario"
+        typeCampo10=""
+
+        campo11="Pregunta:"
+        idCampo11="pregunta"
+        typeCampo11=""
+
+        campo12="Respuesta:"
+        idCampo12="respuesta"
+        typeCampo12="text"
+        
+    />
+
+
+      </div>
     </div>
+
   );
+
 }
 
 export default GestionUsuarios;
