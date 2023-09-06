c32bc0806667f2968020c50a310d29143840a6bc
Moisés Villarroel <140099979+MoisesV2605@users.noreply.github.com>
2023-09-06 01:30:28 -0400

    ID se agrega solo y fix modal con la tabla

--- frontend/src/components/Add/Add.jsx
+++ frontend/src/components/Add/Add.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { useState } from "react";
-
+import { v4 as uuidv4 } from 'uuid';
 
 import "./Add.css";
 
-
 function Add({ estado, cambiarEstado, titulo, campos, onGuardar }) {
-
   const [formData, setFormData] = useState({});
-  const [filas, setFilas] = useState([]); //
+  const [filas, setFilas] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onGuardar(formData);
-    setFilas([...filas, formData]);
+
+    const newFormData = { ...formData, id: uuidv4() };
+
+    onGuardar(newFormData);
+    setFilas([...filas, newFormData]);
     setFormData({});
     cambiarEstado(false);
   };
 
-
-  
+  // Filtrar los campos para excluir el campo ID
+  const camposFiltrados = campos.filter(({ idCampo }) => idCampo !== 'id');
 
   return (
     <>
-      {estado &&
+      {estado && (
         <div className="superposiciones">
           <div className="contenedor-modal">
             <div className="parte-arriba">
@@ -31,19 +32,31 @@ function Add({ estado, cambiarEstado, titulo, campos, onGuardar }) {
                 <h3>{titulo}</h3>
               </div>
               <div>
-                <button className="boton-cerrar-primario" onClick={() => cambiarEstado(!estado)}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                <button
+                  className="boton-cerrar-primario"
+                  onClick={() => cambiarEstado(!estado)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-x"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
+                    />
                   </svg>
                 </button>
               </div>
             </div>
-            <form className="form-grid" onSubmit={handleSubmit} >
-              <div className='formulario-input'>
-                {campos.map(({ campo, idCampo, typeCampo, options }) => (
+            <form className="form-grid" onSubmit={handleSubmit}>
+              <div className="formulario-input">
+                {camposFiltrados.map(({ campo, idCampo, typeCampo, options }) => (
                   <div key={idCampo}>
                     <label htmlFor={idCampo}>{campo}</label>
-                    {typeCampo === 'select' ? (
+                    {typeCampo === "select" ? (
                       <select id={idCampo}>
                         {options.map((option) => (
                           <option key={option} value={option}>
@@ -52,27 +65,37 @@ function Add({ estado, cambiarEstado, titulo, campos, onGuardar }) {
                         ))}
                       </select>
                     ) : (
-                      <input id={idCampo} type={typeCampo} value={formData[idCampo]} onChange={(e) => setFormData({ ...formData, [idCampo]: e.target.value })} />
+                      <input
+                        id={idCampo}
+                        type={typeCampo}
+                        value={formData[idCampo] || ""}
+                        onChange={(e) =>
+                          setFormData({
+                            ...formData,
+                            [idCampo]: e.target.value,
+                          })
+                        }
+                      />
                     )}
                   </div>
                 ))}
+              </div>
+              <div className="botones">
+                <div className="boton-guardar">
+                  <button type="submit">Guardar</button>
                 </div>
-                <div className="botones">
-                  <div className='boton-guardar'>
-                    <button type="submit">Guardar</button>
-                  </div>
 
-                  <div className="boton-cerrar" onClick={() => cambiarEstado(!estado)}>
-                    <button type="button">Cancelar</button>
-                  </div>
+                <div
+                  className="boton-cerrar"
+                  onClick={() => cambiarEstado(!estado)}
+                >
+                  <button type="button">Cancelar</button>
                 </div>
-                
+              </div>
             </form>
-
           </div>
-
         </div>
-      }
+      )}
     </>
   );
 }
