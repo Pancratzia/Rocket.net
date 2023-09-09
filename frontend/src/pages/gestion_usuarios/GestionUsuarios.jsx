67e72ce16e12ab9a48dd1804193a2993b77312c3
Carlos Escalona <140100221+CalOwO278@users.noreply.github.com>
2023-09-09 19:50:57 -0400

    integracion usuario

--- frontend/src/pages/gestion_usuarios/GestionUsuarios.jsx
+++ frontend/src/pages/gestion_usuarios/GestionUsuarios.jsx
@@ -1,14 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import "./GestionUsuarios.css";
 import Tabla from '../../components/Tabla/Tabla';
 import Add from '../../components/Add/Add';
 import Swal from "sweetalert2";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 
 function GestionUsuarios() {
 
+  const [sedeDepartamentoOptions, setSedeDepartamentoOptions] = useState([]);
+
   const [usuarios, setUsuarios] = useState([]);
+
+  const obtenerSedepartamentoOptions = () => {
+    axios.get('http://localhost:3000/api/sedesdepartamentos')
+      .then(response => {
+        if (response.status === 200) {
+          const opciones = response.data.map(opcion => ({
+            value: opcion.id_sede_departamento,
+            label: opcion.Sede_Departamento,
+          }));
+          console.log('Opciones obtenidas de la API:', opciones);
+          setSedeDepartamentoOptions(opciones);
+        } else {
+          console.error('Error al obtener las opciones de sedepartamento:', response);
+        }
+      })  
+      .catch(error => {
+        console.error('Error al obtener las opciones de sedepartamento:', error);
+      });
+  };
+  
+  
+  useEffect(() => {
+    obtenerSedepartamentoOptions();
+  }, []);
 
   const columnas = [
     { field: 'id', headerName: 'ID', width: 40, editable: false },
@@ -34,7 +62,10 @@ function GestionUsuarios() {
       description: 'Esta es la pregunta de seguridad',
       width: 160,
       type: 'select',
-      options: ['RRHH - Barquisimeto', "2", "3"],
+      options: sedeDepartamentoOptions.map(opcion => ({
+        value: opcion.value,
+        label: opcion.label,
+      })),
      },
     {
       field: 'extensiontelefonica',
@@ -58,22 +89,29 @@ function GestionUsuarios() {
       headerName: 'Correo',
       width: 160,
     },
-    {
-      field: 'password',
-      headerName: 'Contrasena',
-      width: 140,
-    },
+
     {
       field: 'pregunta',
       headerName: 'Pregunta',
       width: 130,
     },
+   
+  ];
+
+  const seguridad = [
     {
-      field: 'answer',
+      field: 'clave',
+      headerName: 'Clave',
+      width: 140,
+    },
+    {
+      field: 'respuesta',
       headerName: 'Respuesta',
       width: 160,
     },
   ];
+
+  const todasLasColumnas = [...columnas, ...seguridad];
 
   const [filas, setFilas] = useState([]) //esto es del modal agregar
   const [estadoModal1, cambiarEstadoModal1] = useState(false); //esto es del modal de agregar
@@ -81,27 +119,35 @@ function GestionUsuarios() {
 
   const [showModal, setShowModal] = useState(false);  //Aca manejamos los estados del modal editar
 
-  useEffect(() => {
-    axios.get('http://localhost:3000/api/usuario')
-      .then(response => {
-        const usuariosConId = response.data.map(usuario => ({
-          id: usuario.id_usuario,
-          usuario: usuario.nombre_usuario,
-          nombre: usuario.nombre,
-          apellido: usuario.apellido,
-          pregunta: usuario.pregunta,
-          extensiontelefonica: usuario.extension_telefonica,
-          telefono: usuario.telefono,
-          cedula: usuario.cedula,
-          correo: usuario.correo,
-        }));
-        setUsuarios(usuariosConId);
-        setFilas(usuariosConId);
-      })
-      .catch(error => {
-        console.error('Error al obtener usuarios:', error);
-      });
-  }, []);
+  // Función para obtener los usuarios desde la API
+ const obtenerUsuarios = () => {
+  axios.get('http://localhost:3000/api/usuarios')
+    .then(response => {
+      const usuariosConId = response.data.map(usuario => ({
+        id: usuario.id_usuario,
+        usuario: usuario.nombre_usuario,
+        nombre: usuario.nombre,
+        apellido: usuario.apellido,
+        pregunta: usuario.pregunta,
+        extensiontelefonica: usuario.extension_telefonica,
+        telefono: usuario.telefono,
+        cedula: usuario.cedula,
+        correo: usuario.correo,
+        sedepartamento: usuario.id_sededepar
+      }));
+      setUsuarios(usuariosConId);
+      setFilas(usuariosConId);
+    })
+    .catch(error => {
+      console.error('Error al obtener usuarios:', error);
+    });
+
+};
+
+useEffect(() => {
+  obtenerUsuarios();
+}, []);
+
 
   //handleEditClick nos permite mostrar el modal de la fila seleccionada para el editar
   const handleEditClick = (row) => {
@@ -111,32 +157,94 @@ function GestionUsuarios() {
  
   };
 
+  const handleDeleteClick = (idUsuario) => {
+    Swal.fire({  // mensaje de confirmacion
+      title: '¿Estás seguro?',
+      text: '¡No podrás revertir esto!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Si el usuario confirma la eliminación, se realiza la solicitud PATCH
+        axios.patch(`http://localhost:3000/api/usuarios/${idUsuario}`)
+          .then(response => {
+            if (response.status === 200) {
+              obtenerUsuarios(); 
+              Swal.fire('Eliminado', 'El usuario ha sido eliminado correctamente', 'success');
+            } else {
+              Swal.fire('Error', 'No se pudo eliminar el usuario', 'error');
+            }
+          })
+          .catch(error => {
+            console.error('Error al eliminar el usuario:', error);
+            Swal.fire('Error', 'Ocurrió un error al eliminar el usuario', 'error');
+          });
+      }
+    });
+  };
+  
+
   const handleDeleteRow = (id) => {
     console.log("borrandofila" + id + "en gestion de usuarios");
     const nuevasFilas = filas.filter((fila) => fila.id !== id);
-    setFilas(NuevasFilas);
+    setFilas(nuevasFilas);
+    handleDeleteClick(id);
   }
 
 
 const [camposEditados, setCamposEditados] = useState({});  // aca estaba definiendo para la actualizacion de la fila de la tabla 
+
+const agregarUsuario = (nuevoUsuario) => {
+  const formData = new FormData();
+  const nuevaImagen = '../../public/imagetest/user.png';
+
+  formData.append('nombre_usuario', nuevoUsuario.nombre_usuario);
+  formData.append('nombre', nuevoUsuario.nombre);
+  formData.append('apellido', nuevoUsuario.apellido);
+  formData.append('pregunta', nuevoUsuario.pregunta);
+  formData.append('extension_telefonica', nuevoUsuario.extension_telefonica);
+  formData.append('telefono', nuevoUsuario.telefono);
+  formData.append('cedula', nuevoUsuario.cedula);
+  formData.append('correo', nuevoUsuario.correo);
+  formData.append('id_sededepar', nuevoUsuario.id_sededepar);
+  formData.append('clave', nuevoUsuario.clave);
+  formData.append('respuesta', nuevoUsuario.respuesta);
+  formData.append('fileUsuario', new File(['user.png'], 'user.png'));
+
+  fetch(nuevaImagen)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const archivo = new File([blob], 'user.png', { type: 'image/png' });
+      formData.append('fileUsuario', archivo);
+
+      axios.post('http://localhost:3000/api/usuarios', formData)
+        .then(response => {
+          if (response.status === 201) {
+            const usuarioCreado = response.data;
+            setUsuarios([...usuarios, usuarioCreado]); 
+            setFilas([...filas, usuarioCreado]); 
+            cambiarEstadoModal1(false); 
+            Swal.fire('Usuario creado', 'El usuario se ha creado correctamente', 'success');
+          } else {
+            Swal.fire('Error', 'No se pudo crear el usuario', 'error');
+          }
+        })
+        .catch(error => {
+          console.error('Error al crear el usuario:', error);
+          Swal.fire('Error', 'Ocurrió un error al crear el usuario', 'error');
+        });
+    });
+};
+
+
  
 //esto es para el agregado de las filas con el modal
   const agregarFila = (nuevaFila) => {
-    axios.post('http://localhost:3000/api/usuario', nuevaFila)
-    .then(response => {
-      if (response.status === 201) {
-        setFilas([...filas, nuevaFila]);
-        cambiarEstadoModal1(false);
-        Swal.fire('Usuario creado', 'El usuario se ha creado correctamente', 'success');
-      } else {
-        Swal.fire('Error', 'No se pudo crear el usuario', 'error');
-      }
-    }) 
-    .catch(error => {
-      // Maneja errores de la solicitud si es necesario
-      console.error('Error al crear el usuario:', error);
-      Swal.fire('Error', 'Ocurrió un error al crear el usuario', 'error');
-    });
+    
+    setCamposEditados({}); // Restablece los campos editados
+    cambiarEstadoModal1(true); // Abre el modal de agregar usuario
   };
 
 
@@ -150,7 +258,8 @@ const [camposEditados, setCamposEditados] = useState({});  // aca estaba definie
           <hr />
         </div>
         <div className='contenedor-busqueda'> 
-          <button className='boton-usuarios' onClick={() => cambiarEstadoModal1(!estadoModal1)}>Agregar</button>
+          <button className='boton-usuarios' onClick={agregarFila}>Agregar</button>
+
           
         </div>
         <Add
@@ -175,8 +284,8 @@ const [camposEditados, setCamposEditados] = useState({});  // aca estaba definie
           estado={estadoModal1}
           cambiarEstado={cambiarEstadoModal1}
           titulo="Agregar usuario"
-          campos={columnas.map(({ headerName: campo, field: idCampo, type, options }) => {
-            if (type === 'select') {
+          campos={todasLasColumnas.map(({ headerName: campo, field: idCampo, type, options }) => {
+            if (type === 'select' ) {
               return {
                 campo,
                 idCampo,
@@ -192,8 +301,10 @@ const [camposEditados, setCamposEditados] = useState({});  // aca estaba definie
           
           filas={filas}
           setFilas={setFilas}
-          onGuardar={agregarFila}
-
+          onGuardar={(nuevoUsuario) => {
+            agregarUsuario(nuevoUsuario); // Llama a la función para agregar el usuario
+            cambiarEstadoModal1(false); // Cierra el modal
+          }}
         />
 
 <Tabla
