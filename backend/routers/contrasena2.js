c6f752ba3268719fcb33ae890ea2c6c2809e835b
JEPerezGomez <je.perezgomez@yahoo.com>
2023-09-20 23:27:06 -0400

    Api contraseña modificada

--- backend/routers/contrasena2.js
+++ backend/routers/contrasena2.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('../database/db.js'); // Asegúrate de tener tu configuración de base de datos correctamente importada
+const pool = require('../database/db.js'); 
 const routerContrasena2 = express.Router();
 
 routerContrasena2.use(express.json());
 routerContrasena2.use(cors());
 
-// Ruta para actualizar la clave por correo electrónico
+// Ruta para actualizar la clave por usuario
 routerContrasena2.put('/', async (req, res) => {
   try {
-    const { correo, nueva_clave } = req.body;
+    const { usuario, nueva_clave } = req.body;
 
-    if (!correo || !nueva_clave) {
+    if (!usuario || !nueva_clave) {
       return res.status(400).json({ error: 'Faltan datos requeridos' });
     }
 
     // Realiza la actualización en la base de datos
-    const updateQuery = 'UPDATE usuarios SET clave = $1 WHERE "correo" = $2';
-    const updateValues = [nueva_clave, correo];
+    const updateQuery = 'UPDATE usuarios SET clave = $1 WHERE "nombre_usuario" = $2';
+    const updateValues = [nueva_clave, usuario];
 
     const result = await pool.query(updateQuery, updateValues);
 
     if (result.rowCount === 0) {
-      return res.status(404).json({ error: 'No se encontró ningún usuario con ese correo electrónico' });
+      return res.status(404).json({ error: 'No se encontró el usuario' });
     }
 
     res.json({ mensaje: 'Clave actualizada correctamente' });
   }  catch (error) {
-    console.error(error); // Agrega esta línea para imprimir el error en la consola
+    console.error(error);
     res.status(500).json({ error: 'Ha ocurrido un error' });
   }
 });
