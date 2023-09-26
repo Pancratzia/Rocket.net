10e6838e22e40cb8775b8904ff70a4acfaefcb8d
csrosibel <140108986+csrosibel@users.noreply.github.com>
2023-09-26 02:12:20 -0400

    Modificacion y Avance del modulo de Reportes

--- frontend/src/pages/reportes/Reportes.jsx
+++ frontend/src/pages/reportes/Reportes.jsx
@@ -6,8 +6,31 @@ import Tabla from '../../components/Tabla/Tabla';
 function Reportes() {
 
     const [opcionSeleccionada, setOpcionSeleccionada] = useState(); //para manejar los estados del select
-    const [columnas, setColumnas] = useState([]); //para manejar los estados de las columnas
-    const [filas, setFilas] = useState([]); //para manejar los estados de las filas
+    const [columnas, setColumnas] = useState([]); //para manejar los estados de las columnas de lasnpm tablas
+    
+
+      // FILAS ESTATICAS PARA CADA TABLA
+
+  //Filas para el reporte de clientes
+    const [filasClientes, setFilasClientes] = useState([
+    { id:1, nombre: "cliente 01", ubicacion: "Ubicación 1", telefono: "1234", correo: "cliente1@gmail.com", plan: "Plan 1", estado: "Activo" },
+    { id:2, nombre: "cliente 02", ubicacion: "Ubicación 2", telefono: "5678", correo: "cliente2@gmail.com", plan: "Plan 2", estado: "Inactivo" }
+   
+  ]);
+
+  //Filas para el reporte de planes
+    const [filasPlanes, setFilasPlanes] = useState([
+    {id: 1, nombre: "Plan 1", descripcion: "50 mbps", precio: "50$", estado: "Activo" },
+    {id: 2, nombre: "Plan 2", descripcion: "100 mbps", precio: "80$", estado: "Inactivo" }
+
+  ]);
+  
+  //Filas para el reporte de usuarios
+    const [filasUsuarios, setFilasUsuarios] = useState([
+    {id: 1, nombre:"Pepito", usuario: "usuario 01", tipoUsuario: "Usuario tipo 1", sedeDepar: "Barquisimeto - RRHH", correo: "pepito@gmail.com" , telefono: "1234"},
+    {id: 2, nombre:"Pepita", usuario: "usuario 02", tipoUsuario: "Usuario tipo 2", sedeDepar: "Barquisimeto - Ventas", correo: "pepita@gmail.com" , telefono: "5678"}
+
+  ]);
 
     //funcion para la seleccion de las opciones del select y que campos deben aparecen en la tabla
     const handleSelect = (e) => {
@@ -15,6 +38,8 @@ function Reportes() {
 
         if (opcion === "opcion-clientes") {
             const columnas = [
+
+                { field: "id", headerName: "ID", width: 40, editable: false },
               
                 {
                   field: "nombre",
@@ -51,18 +76,29 @@ function Reportes() {
                 {
                   field: "estado",
                   headerName: "Estado de Servicio",
-                  width: 200
+                  width: 200,
+                  cellClassName: (params) => {
+                    if (params.value === "Activo") { 
+                      return 'estado-activo';
+                    } else if (params.value === "Inactivo") {
+                      return 'estado-inactivo'; 
+                    }
+                    return '';
+                  }, 
                   
                 }
                 
                 ];
                
-                setColumnas(columnas);
+                setColumnas(columnas); //actualiza las columnas
+                setFilasClientes([...filasClientes]);  //actualiza las filas
             
                 } 
 
         else if (opcion === "opcion-planes") {
             const columnas = [  
+
+                { field: "id", headerName: "ID", width: 40, editable: false },
              
                 {
                   field: "nombre",
@@ -73,7 +109,7 @@ function Reportes() {
                 {
                   field: "descripcion",
                   headerName: "Descripcion",           
-                  width: 350
+                  width: 300
                 },
             
                 {
@@ -85,17 +121,29 @@ function Reportes() {
                 {
                   field: "estado",
                   headerName: "Estado de Servicio",
-                  width: 300
+                  width: 300,
+                  //ESTO ES PARA MOSTRAR EL BACKGROUND ROJO O VERDE DEPENDIENDO DEL ESTADO
+                  cellClassName: (params) => {
+                    if (params.value === "Activo") { 
+                      return 'estado-activo';
+                    } else if (params.value === "Inactivo") {
+                      return 'estado-inactivo'; 
+                    }
+                    return '';
+                  }, 
                 
                 },
                 ];
                 setColumnas(columnas);
+                setFilasPlanes([...filasPlanes]); 
 
              
                 } 
         
             else if (opcion === "opcion-usuarios") {
                 const columnas = [
+
+                { field: "id", headerName: "ID", width: 40, editable: false },  
                     
                 {
                   field: "nombre",
@@ -111,13 +159,13 @@ function Reportes() {
                 },
                 
                 {
-                  field: "tipo-usuario",
+                  field: "tipoUsuario",
                   headerName: "Tipo de Usuario",
                   width: 220
                 },
 
                 {
-                  field: "sede-depar",
+                  field: "sedeDepar",
                   headerName: "Sede - Departamento",
                   width: 300
                 },
@@ -136,6 +184,7 @@ function Reportes() {
                     
                 ];
                 setColumnas(columnas); 
+                setFilasUsuarios([...filasUsuarios]); 
          
                  }
                     setOpcionSeleccionada(opcion); // aca se actualiza el estado de la opcion seleccionada del select
@@ -158,9 +207,9 @@ function Reportes() {
         </select>
         </div>
        
-        {(opcionSeleccionada === "opcion-clientes") && <Tabla columns={columnas} rows={filas} />}
-        {(opcionSeleccionada === "opcion-planes") && <Tabla columns={columnas} rows={filas} />}
-        {(opcionSeleccionada === "opcion-usuarios") && <Tabla columns={columnas} rows={filas} />}
+        {(opcionSeleccionada === "opcion-clientes") && <Tabla columns={columnas} rows={filasClientes} />}
+        {(opcionSeleccionada === "opcion-planes") && <Tabla columns={columnas} rows={filasPlanes} />}
+        {(opcionSeleccionada === "opcion-usuarios") && <Tabla columns={columnas} rows={filasUsuarios} />}
         </div>
     )
 }
