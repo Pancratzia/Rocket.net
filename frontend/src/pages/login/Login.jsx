758b8adc2bd29b138964faa6366ab7e2d874df47
Juan Villegas <juancvillefer@gmail.com>
2023-09-29 17:58:42 -0400

    integracion de sesion-auditorias

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -15,8 +15,8 @@ function Login() {
     customClass: {
       confirmButton: 'btn btn-success',
       cancelButton: 'btn btn-danger'
-      },
-      buttonsStyling: false
+    },
+    buttonsStyling: false
   })
 
   const manejarLogin = (e) => {
@@ -32,15 +32,9 @@ function Login() {
       .then((response) => {
         if (response.data.jwt) {
           localStorage.setItem("jwt", response.data.jwt);
-          // localStorage.setItem("usuarioenSesion", response.data.idUser)
-          // setTimeout(() => {
-          //   Swal.fire("Éxito", "Inicio de sesión exitoso", "success");
-          //   window.location.href = '/';
-          // }, 60000);
-            Swal.fire("Éxito", "Inicio de sesión exitoso", "success");
-            window.location.href = '/';
+          Swal.fire("Éxito", "Inicio de sesión exitoso", "success");
+          window.location.href = '/';
         }
-        // console.log(response)
       })
       .catch((error) => {
         Swal.fire("Error", "Hubo un error en el inicio de sesión", "error");
@@ -51,39 +45,39 @@ function Login() {
 
 
   return (
-      <div className="contenedor-login">
-        <div className="contenedor-form">
-          <div className="titulo">
-            <h1>Bienvenido</h1>
-            <h2>Inicia sesión para continuar</h2>
-          </div>
-
-          <form className="form" method="post">
-            <label className="label" htmlFor="usuario">Usuario</label>
-            <input
-              name="usuario"
-              id="usuario"
-              type="text"
-              className="input"
-              value={usuario}
-              onChange={(e) => setUsuario(e.target.value)}
-            />
-            <label className="label" htmlFor="password">Contraseña</label>
-            <input
-              name="password"
-              id="password"
-              type="password"
-              className="input"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="button" className="btn" onClick={manejarLogin}>Iniciar sesión</button>
-            
-          </form>
+    <div className="contenedor-login">
+      <div className="contenedor-form">
+        <div className="titulo">
+          <h1>Bienvenido</h1>
+          <h2>Inicia sesión para continuar</h2>
         </div>
-       
-        <Link to="/recuperar_password" className="link">¿Olvidaste la contraseña?</Link>
+
+        <form className="form" method="post">
+          <label className="label" htmlFor="usuario">Usuario</label>
+          <input
+            name="usuario"
+            id="usuario"
+            type="text"
+            className="input"
+            value={usuario}
+            onChange={(e) => setUsuario(e.target.value)}
+          />
+          <label className="label" htmlFor="password">Contraseña</label>
+          <input
+            name="password"
+            id="password"
+            type="password"
+            className="input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="button" className="btn" onClick={manejarLogin}>Iniciar sesión</button>
+
+        </form>
       </div>
+
+      <Link to="/recuperar_password" className="link">¿Olvidaste la contraseña?</Link>
+    </div>
   );
 }
 
